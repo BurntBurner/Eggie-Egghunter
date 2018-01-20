344504bee0c6dc531b5fd8cdb68df6d717b5f955
Unknown <floriangaebler87@gmail.com>
2018-01-20 14:39:43 +0100

    CleanUp
    New  Parallax Setup

---
+++ Assets/parallax/Scripts/Scrolling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour {
+
+    Vector2 uvOffset = Vector2.zero;
+    //public string textureName = "_MainTex";
+    public float uvSpeedY;
+    public float uvSpeedX;
+    public Material Mat;
+
+    private void Start()
+    {
+        Mat = GetComponent<SpriteRenderer>().material;
+        
+    }
+
+    void LateUpdate()
+    {
+
+        Vector2 uvAnimSpeed = new Vector2(uvSpeedX, uvSpeedY);
+
+        uvOffset += (uvAnimSpeed * Time.deltaTime);
+        //if( GetComponent<Renderer>().enabled )
+        //{
+        Mat.SetTextureOffset("_MainTex", uvOffset);
+        //}
+
+    }
+}
+
