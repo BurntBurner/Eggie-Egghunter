d9f41fb6b260575e2ad6e26cbb72e6b56923832e
Taskman <bernd.limberg@gmx.de>
2018-03-15 23:39:29 +0100

    UI Start
    Supershiny Buttons! 
    Watch it :)

---
+++ Assets/loadSceneGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;	
+using UnityEngine.SceneManagement;
+
+public class loadSceneGame: MonoBehaviour {
+	
+	public void Ficken()
+	{
+
+		SceneManager.LoadScene ("Egghunter_alpha_5b1");
+
+	}
+	}
