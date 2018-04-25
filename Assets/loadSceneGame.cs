785a84588366e41fa0cad9cff49ad117ee89aa7b
Taskman <bernd.limberg@gmx.de>
2018-04-25 21:15:55 +0200

    Title Timeline included

--- Assets/loadSceneGame.cs
+++ Assets/loadSceneGame.cs
@@ -7,7 +7,7 @@ public class loadSceneGame: MonoBehaviour {
 	public void Ficken()
 	{
 
-		SceneManager.LoadScene ("Egghunter_alpha_5c2");
+		SceneManager.LoadScene ("Egghunter_alpha_6a");
 
 	}
 	}
